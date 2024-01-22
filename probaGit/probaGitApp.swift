c41e0c75bb598bcc059adb29f2dee4143573682f
Sheila <sheila.feijoo@iessanmamede.com>
2024-01-22 11:05:55 +0100

    Initial Commit

---
+++ probaGit/probaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  probaGitApp.swift
+//  probaGit
+//
+//  Created by Alumnado on 22/1/24.
+//
+
+import SwiftUI
+
+@main
+struct probaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
