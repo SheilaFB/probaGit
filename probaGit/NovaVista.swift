6544cb7ff50f61e6b53f8cbd31fba2e6c42fec40
Sheila <sheila.feijoo@iessanmamede.com>
2024-01-22 11:36:30 +0100

    nova vista

---
+++ probaGit/NovaVista.swift
@@ -0,0 +1,20 @@
+//
+//  NovaVista.swift
+//  probaGit
+//
+//  Created by Alumnado on 22/1/24.
+//
+
+import SwiftUI
+
+struct NovaVista: View {
+    var body: some View {
+        Text("Lerolorelore")
+    }
+}
+
+struct NovaVista_Previews: PreviewProvider {
+    static var previews: some View {
+        NovaVista()
+    }
+}
